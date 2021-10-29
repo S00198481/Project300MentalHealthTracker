4f6ff98fed72dc8cadf1267bc20a0afbb4d75d02
CianTivnan <cian.tivnan@yahoo.ie>
2021-10-29 21:11:34 +0100

    recorder component initialised

---
+++ src/app/recorder/recorder.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-recorder',
+  templateUrl: './recorder.component.html',
+  styleUrls: ['./recorder.component.css']
+})
+export class RecorderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  record() {
+    
+  }
+
+}
