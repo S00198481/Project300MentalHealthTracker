216d645f2df6fbb439aa2a38920dbf3e34ea8e95
CianTivnan <cian.tivnan@yahoo.ie>
2021-11-10 18:52:06 +0000

    aws upload errors removed, functionally complete

--- src/app/recorder/recorder.component.ts
+++ src/app/recorder/recorder.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { VoiceRecognitionService } from '../service/voice-recognition.service';
 import { SentimentApiService } from '../service/sentiment-api.service';
 import { AWSService } from '../service/aws.service';
+import { waitForAsync } from '@angular/core/testing';
 
 @Component({
   selector: 'app-recorder',
@@ -12,71 +13,8 @@ export class RecorderComponent implements OnInit {
 
   recording: boolean = false;
   userText: string;
-  emotion: any = {
-    "emotions_detected": [
-        "anger",
-        "fear",
-        "disgust",
-        "sadness"
-    ],
-    "emotion_scores": {
-        "anger": 0.08139157539877298,
-        "fear": 0.07724855396303418,
-        "disgust": 0.0565423309376263,
-        "sadness": 0.05185282182503058,
-        "surprise": 0.017367632925249947,
-        "joy": 0
-    },
-    "thresholds": {
-        "disgust": 0.05,
-        "sadness": 0.05,
-        "anger": 0.05,
-        "joy": 0.05,
-        "surprise": 0.05,
-        "fear": 0.05
-    },
-    "version": "7.0.7",
-    "author": "twinword inc.",
-    "email": "help@twinword.com",
-    "result_code": "200",
-    "result_msg": "Success"
-};
-  sentiment: any = ({
-    "type": "negative",
-    "score": -0.393845528,
-    "ratio": -0.7835246324260892,
-    "keywords": [
-        {
-            "word": "very",
-            "score": 0.192192596
-        },
-        {
-            "word": "day",
-            "score": 0.13424713
-        },
-        {
-            "word": "bad",
-            "score": -1.488950796
-        },
-        {
-            "word": "sad",
-            "score": -0.97837309
-        },
-        {
-            "word": "have",
-            "score": -0.162909152
-        },
-        {
-            "word": "feeling",
-            "score": -0.059279856
-        }
-    ],
-    "version": "7.0.8",
-    "author": "twinword inc.",
-    "email": "help@twinword.com",
-    "result_code": "200",
-    "result_msg": "Success"
-});
+  emotion: any;
+  sentiment: any;
   errorMessage: any;
 
   constructor(
@@ -106,9 +44,21 @@ export class RecorderComponent implements OnInit {
   }
 
   submitData() {
-    /*this.userText = document.getElementById("text").textContent
+    this.processData();
+    setTimeout(() => {
+      this.userText = this.userText.split("%20").join(" ");
+      this.emotion = JSON.stringify(this.emotion)
+      this.sentiment = JSON.stringify(this.sentiment)
+      this.AWSService.sendData(this.userText, this.emotion, this.sentiment);
+     }, 10000);
+
+  }
+
+  processData() {
+    this.userText = document.getElementById("text").textContent
     this.userText = this.userText.split(" ").join("%20");
-    this.sentimentService.getEmotion(this.userText).subscribe(
+    
+  this.sentimentService.getEmotion(this.userText).subscribe(
       emotion => {
         this.emotion = emotion;
       },
@@ -120,11 +70,9 @@ export class RecorderComponent implements OnInit {
         this.sentiment = sentiment;
       },
       error => this.errorMessage = <any>error
-    );*/
+    );
 
     this.emotion = JSON.stringify(this.emotion)
     this.sentiment = JSON.stringify(this.sentiment)
-
-    this.AWSService.sendData("hello!", this.emotion, this.sentiment)
   }
 }
--- src/app/service/aws.service.ts
+++ src/app/service/aws.service.ts
@@ -3,8 +3,8 @@ import { HttpClient, HttpErrorResponse } from "@angular/common/http";
 import { Observable } from 'rxjs';
 import { catchError, tap } from 'rxjs/operators';
 import { DynamoDB } from 'node_modules/aws-sdk';
-
 import { Credentials } from 'node_modules/aws-sdk';
+import { v4 as uuidv4 } from 'uuid'
 
 
 @Injectable({
@@ -13,8 +13,9 @@ import { Credentials } from 'node_modules/aws-sdk';
 export class AWSService {
 
   client = new DynamoDB({region: "eu-west-1"});
-  cred = new Credentials("AKIAWDJHI5VVRKEITGNG", "6sm+skj7bhPe3eTcdirE2YSgI0Gol8f4615EZ5QS");
+  cred = new Credentials("", "");
   data:any;
+  key:any;
 
   params:any;
 
@@ -22,11 +23,16 @@ export class AWSService {
 
   async sendData(text:string, emotion:string, sentiment:string) {
     this.client.config.region = "eu-west-1"
-    this.client.config.credentials = this.cred;
+    this.client.config.credentials = this.cred
     this.client.config.update({region: "eu-west-1"})
+    this.key = uuidv4()
+    console.log(this.key)
     this.params = {
       TableName: "AppRecordings",
       Item: {
+        "UUID": {
+          S: this.key
+        },
         "UserID": {
           S: "username"
         },
