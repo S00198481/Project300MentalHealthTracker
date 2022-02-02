36effc61ebebe9619e93788628caf97943cee22c
Keanan McDonald <keananmcdonald5@gmail.com>
2022-02-02 20:11:48 +0000

    Activities now being stored in array on click

--- src/app/activity/activity.component.ts
+++ src/app/activity/activity.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { threadId } from 'worker_threads';
+import { AWSService } from '../service/aws.service';
 
 @Component({
   selector: 'app-activity',
@@ -7,35 +9,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ActivityComponent implements OnInit {
 
-  activity: string[] = null;
+  activity: string[] = new Array();
 
-  constructor() { }
+  constructor(public AWSService : AWSService) { }
 
   ngOnInit(): void {
-
   }
 
-  handleActivities(buttonNumber: number): void {
+  handleActivities(buttonNumber: number) {
     switch(buttonNumber) {
       case 1:
         this.activity.push("Running");
+        console.log(this.activity)
         break;
       case 2:
         this.activity.push("Walking");
+        console.log(this.activity)
         break;
       case 3:
         this.activity.push("Swimming");
+        console.log(this.activity)
         break;
       case 4:
-        this.activity.push("Cycling");
+       this.activity.push("Cycling");
+        console.log(this.activity)
         break;
       case 5:
         this.activity.push("Cumming");
+        console.log(this.activity)
         break;
       default:
         console.log("No activity undertaken.");
+        console.log(this.activity)
         break;
     }
+  }
+
+  submitActivityData() {
   }
 
 }
